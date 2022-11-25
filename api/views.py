24300608544ec1a70cb98b85997de4b4e92ebd26
Lyle-Rogers <lylerogers8@gmail.com>
2022-11-24 21:50:28 -0700

    I added the black code formatter!

--- api/views.py
+++ api/views.py
@@ -10,54 +10,56 @@ from .serializers import *
 from .throttling import *
 from .permissions import *
 
+
 @permission_classes([AllowAny])
 class LoginView(APIView):
-  # throttle_classes = []
+    # throttle_classes = []
 
-  def post(self, request):
-    username = request.data['username']
-    password = request.data['password']
+    def post(self, request):
+        username = request.data["username"]
+        password = request.data["password"]
 
-    try:
-      user = User.objects.get(username=username)
-    except:
-      return Response('Invalid username')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            return Response("Invalid username")
 
-    if not user.check_password(password):
-      return Response('Invalid password')
+        if not user.check_password(password):
+            return Response("Invalid password")
 
-    auth_token = Token.objects.get_or_create(user = user.id)
+        auth_token = Token.objects.get_or_create(user=user.id)
 
-    return Response(
-      {"user_id": user.id, "auth_token": str(auth_token[0])},
-      status = status.HTTP_200_OK
-    )
+        return Response(
+            {"user_id": user.id, "auth_token": str(auth_token[0])},
+            status=status.HTTP_200_OK,
+        )
+
 
 @permission_classes([AllowAny])
 class RegisterView(APIView):
-  # throttle_classes = []
+    # throttle_classes = []
 
-  def post(self, request):
-    username = request.data['username']
+    def post(self, request):
+        username = request.data["username"]
 
-    username_registered = User.objects.get(username=username)
+        username_registered = User.objects.get(username=username)
 
-    if username_registered:
-      return Response('Username is already registered')
+        if username_registered:
+            return Response("Username is already registered")
 
-    registerSerializer = RegisterSerializer(data = request.data)
-    registerSerializer.is_valid(raise_exception = True)
-    registerSerializer.save()
+        registerSerializer = RegisterSerializer(data=request.data)
+        registerSerializer.is_valid(raise_exception=True)
+        registerSerializer.save()
 
-    user = User.objects.get(id = registerSerializer.data['id'])
-    
-    userPropertiesSerializer = UserPropertiesSerializer(data = None, partial = True)
-    userPropertiesSerializer.is_valid(raise_exception = True)
-    userPropertiesSerializer.save(user = user)
+        user = User.objects.get(id=registerSerializer.data["id"])
 
-    auth_token = Token.objects.create(user = user)
+        userPropertiesSerializer = UserPropertiesSerializer(data=None, partial=True)
+        userPropertiesSerializer.is_valid(raise_exception=True)
+        userPropertiesSerializer.save(user=user)
 
-    return Response(
-      {'user_id': user.id, 'auth_token': auth_token.key},
-      status = status.HTTP_200_OK
-    )
+        auth_token = Token.objects.create(user=user)
+
+        return Response(
+            {"user_id": user.id, "auth_token": auth_token.key},
+            status=status.HTTP_200_OK,
+        )
